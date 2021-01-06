da0d7dd084c14cd7bbb06b1d1fd348660e6713da
Pelzvieh <pelzi@flying-snail.de>
2021-01-06 01:05:43 +0100

    Changed to pigpio

--- src/dht_updated.py
+++ src/dht_updated.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import time
-import board
-import adafruit_dht
 import datetime
+from pigpio_dht import DHT22
+
+gpio=4
 
 # Initial the dht device, with data pin connected to:
 try:
-    dhtDevice = adafruit_dht.DHT22(board.D4)
+    dhtDevice = DHT22(gpio)
 except RuntimeError as error:
     print("Init failed, aborting to read out: {}".format(error.args[0]))
     sys.exit(1)
@@ -15,22 +16,20 @@ attempt = 1
 while True:
     try:
         success = False
-        temperature_c = dhtDevice.temperature
-        humidity = dhtDevice.humidity
+        result = dhtDevice.read()
         timestamp = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        success = result['valid']
+        if success:
+            print(
+                "{{\n    \"temperature\": {temperature_c:.1f},\n    \"humidity\": {humidity},\n    \"timestamp\": \"{timestamp}\"\n}}\n".format(
+                  temperature_c=result['temp_c'], humidity=result['humidity'], timestamp=timestamp
+                ),
+                file=open('/var/lib/dht_updated/dht.json', 'w')
+            )
 
-        print(
-            "{{\n    \"temperature\": {temperature_c:.1f},\n    \"humidity\": {humidity},\n    \"timestamp\": \"{timestamp}\"\n}}\n".format(
-              temperature_c=temperature_c, humidity=humidity, timestamp=timestamp
-            ),
-            file=open('/var/lib/dht_updated/dht.json', 'w')
-        )
-
-        success=True
         attempt=1
 
     except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
         if attempt > 10:
             print("{}: {}".format(attempt, error.args[0]))
         attempt=attempt+1
