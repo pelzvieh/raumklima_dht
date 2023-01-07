f5065f2b83b23cfbe8b4c0c9339bdc518245aa12
Andreas Feldner <pelzi@flying-snail.de>
2023-01-07 01:38:05 +0100

    Rewritten to use dht11 kernel module, dropping any dependency on pigpio.

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # Raumklima_DHT
 
 This is a extremely simple daemon that reads temperature and humidity from a DHT22 sensor using
-Adafruit library. It writes the readings in a json file. This is intended to be referred from a
+the dht11 kernel module. It writes the readings in a json file. This is intended to be referred from a
 web server, providing a poor man's temperature/humidity API.
 
+# Setup
+
+It is required to set up the dht11 module by configuring it as dtoverlay. This would be achieved
+by adding the following setting to /boot/config.txt in the example if the sensor
+is connected to GPIO 4
+````
+dtoverlay=dht11,gpiopin=4
+````
 
 # Incomplete
 
--- src/dht_updated.py
+++ src/dht_updated.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python3
+import sys
 import time
 import datetime
-from pigpio_dht import DHT22
+import pathlib
 
-gpio=4
-
-# Initial the dht device, with data pin connected to:
-try:
-    dhtDevice = DHT22(gpio, timeout_secs=5)
-except RuntimeError as error:
-    print("Init failed, aborting to read out: {}".format(error.args[0]))
+platform_dir = pathlib.PosixPath('/sys/devices/platform/')
+if not platform_dir.is_dir():
+    print("/sys/devices/platform/ is not a directory", file=sys.stderr)
     sys.exit(1)
+dht11_device_path = next(next(platform_dir.glob('dht11*')).glob('iio:device*'))
+temperature_path = dht11_device_path / 'in_temp_input'
+humidity_path = dht11_device_path / 'in_humidityrelative_input'
+if not temperature_path.is_file() or not humidity_path.is_file():
+    print(f"No char device files found at {temperature_path} or {humidity_path}", file=sys.stderr)
+    sys.exit(2)
 
 attempt = 1
 while True:
     try:
         success = False
-        result = dhtDevice.read()
+        with open(temperature_path, "r") as temperature_file:
+            temperature_c = float(next(temperature_file)) / 1000.0
+        with open(humidity_path, "r") as humidity_file:
+            humidity = float(next(humidity_file)) / 1000.0
         timestamp = datetime.datetime.now().timestamp()
-        success = result['valid']
-        if success:
+        success = True
+        with open('/var/lib/dht_updated/dht.json', 'w') as output_file:
             print(
-                "{{\n    \"temperature\": {temperature_c},\n    \"humidity\": {humidity},\n    \"timestamp\": \"{timestamp}\"\n}}\n".format(
-                  temperature_c=result['temp_c'], humidity=result['humidity'], timestamp=timestamp
-                ),
-                file=open('/var/lib/dht_updated/dht.json', 'w')
+                f"{{\n    \"temperature\": {temperature_c},\n    \"humidity\": {humidity},\n"
+                f"    \"timestamp\": \"{timestamp}\"\n}}\n",
+                file=output_file
             )
-
         attempt=1
 
     except Exception as error:
         if attempt > 10:
-            print("{}: {}".format(attempt, error.args[0]))
+            print("{}: {}".format(attempt, error))
         attempt=attempt+1
 
-    time.sleep(60.0 if success else 2.0)
+    time.sleep(60.0 if success else 1.0)
